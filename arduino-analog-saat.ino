b049f829796ca261734cfbaa4dc6d45d729a27d1
Evren EFE <efeevren@gmail.com>
2016-01-13 23:31:47 +0200

    r0.2
    Multiplexing düzeltildi, DS3231'den saat okuma eklendi

--- arduino-analog-saat.ino
+++ arduino-analog-saat.ino
@@ -7,7 +7,13 @@
  *
  * Version History:
  * 0.1 - Temel multiplexing yapıldı.
+ * 0.2 - Multiplexing düzeltildi, DS3231'den saat okuma eklendi
  */
+
+#include <DS3231.h>
+#include <Wire.h>
+ 
+DS3231 Clock;
 
 //Dakika
 //Pin connected to ST_CP of 74HC595
@@ -17,94 +23,81 @@ const byte clockPin = 3;
 //Pin connected to DS of 74HC595
 const byte dataPin = 4;
 
-//Saat
-//Pin connected to ST_CP of 74HC595
-const byte slatchPin = 8;
-//Pin connected to SH_CP of 74HC595
-const byte sclockPin = 6;
-//Pin connected to DS of 74HC595
-const byte sdataPin = 7;
+//Saat ledleri matrix
+byte saatler[13] = {
+ //KKKKAAAA
+   B11110000,  // 0-none
+   B11100001,  // 1
+   B11010001,  // 2
+   B10110001,  // 3
+   B01110001,  // 4
+   B11100010,  // 5
+   B11010010,  // 6
+   B10110010,  // 7
+   B01110010,  // 8
+   B11100100,  // 9
+   B11010100,  // 10
+   B10110100,  // 11
+   B01110100  // 12
+}; //kod yazmaya erindim
 
-
-unsigned long dpreviousMillis = 0;
-unsigned long spreviousMillis = 0;
-const long dinterval = 100;
-const long sinterval = 4500;
+unsigned long previousMillis = 0;
+const long interval = 1000;
 byte sa = 1;
 byte da = 0;
+byte sn = 0;
+
+
 void setup()
  { 
+  Serial.begin(9600);
   pinMode(dataPin, OUTPUT);
   pinMode(clockPin, OUTPUT);
   pinMode(latchPin, OUTPUT);
-  pinMode(sdataPin, OUTPUT);
-  pinMode(sclockPin, OUTPUT);
-  pinMode(slatchPin, OUTPUT);
   
-/*
-  digitalWrite(latchPin, LOW);
-  //U_A
-  shiftOut(dataPin, clockPin, MSBFIRST, B00000000); 
 
-  //U_K
-  shiftOut(dataPin, clockPin, MSBFIRST, B11111111); 
-  digitalWrite(latchPin, HIGH);
-*/
-  saat(sa);
-  dakika(da);
  }
-
+ 
+void ReadDS3231()
+{
+  bool h12;
+  bool PM;
+  sn=Clock.getSecond();
+  da=Clock.getMinute();
+  sa=Clock.getHour(h12, PM);
+  sa = sa % 12;
+  Serial.print(sa,DEC);
+  Serial.print(':');
+  Serial.print(da,DEC);
+  Serial.print(':');
+  Serial.println(sn);
+}
+ 
 void loop()
 {
  unsigned long currentMillis = millis();
-  if(currentMillis - dpreviousMillis >= dinterval) {
-    dpreviousMillis = currentMillis; 
-    da++;
-    if (da >= 60) { da = 0; }
-    dakika(da);
+  if(currentMillis - previousMillis >= interval) {
+   previousMillis = currentMillis; 
+   ReadDS3231();
+   kadranGuncelle();
   } 
- if(currentMillis - spreviousMillis >= sinterval) {
-    spreviousMillis = currentMillis; 
-    sa++;
-    if (sa >= 13) { sa = 1; }
-    saat(sa);
-  }
 }
 
-void dakika(byte dk) {
+void kadranGuncelle(){
  byte ua = 0;
  byte uk = 0;
  byte a = 0;
  byte k = 0;
 
- a = dk / 8;
- k = dk % 8;
+ a = da / 8;
+ k = da % 8;
  ua = 1 << a;
  uk = B11111111 ^ ( 1 << k );
+ 
  digitalWrite(latchPin, LOW);
+ shiftOut(dataPin, clockPin, MSBFIRST, saatler[sa]); 
  shiftOut(dataPin, clockPin, MSBFIRST, ua); 
  shiftOut(dataPin, clockPin, MSBFIRST, uk); 
  digitalWrite(latchPin, HIGH);
-}
-
-void saat(byte sa) {
- byte saatler[13] = {
-  B00001111,  // 0-none
-  B10000111,  // 1
-  B10001011,  // 2
-  B10001101,  // 3
-  B10001110,  // 4
-  B01000111,  // 5
-  B01001011,  // 6
-  B01001101,  // 7
-  B01001110,  // 8
-  B00100111,  // 9
-  B00101011,  // 10
-  B00101101,  // 11
-  B00101110   // 12
- }; //kod yazmaya erindim
- digitalWrite(slatchPin, LOW);
- shiftOut(sdataPin, sclockPin, LSBFIRST, saatler[sa]); 
- digitalWrite(slatchPin, HIGH);
 }
 
